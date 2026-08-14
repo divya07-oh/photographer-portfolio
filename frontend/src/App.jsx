a126bf2251551b5e3fa339c0a029066e9a018b08
DIVYA <divyavenkatesan239@gmail.com>
2026-08-14 21:24:06 +0530

    feat: configure application routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import ProjectDetails from './pages/ProjectDetails';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
+// Scroll to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="portfolio/:projectId" element={<ProjectDetails />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
